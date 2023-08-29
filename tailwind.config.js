8ab3ba9b1a57eead4508f50a049025fcdfd71714
Yuvadi29 <adityatrivedi612@gmail.com>
2023-08-29 18:35:50 +0530

    Dynamically Change according to Temperature with hot and rainy temperature

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'hot' : 'url("https://weather-app-yuvadi29.vercel.app/static/media/hot.9a2d2c8984d58c325d0f.jpg")'
+        'hot' : 'url("https://weather-app-yuvadi29.vercel.app/static/media/hot.9a2d2c8984d58c325d0f.jpg")',
+        'rain' : 'url("https://c4.wallpaperflare.com/wallpaper/860/775/772/miui-8-weather-background-minimal-rainy-wallpaper-preview.jpg")'
       }
     },
   },
